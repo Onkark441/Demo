5ce0c576e12e4e3d768a887aaa6651a2e150fe87
Onkar Kulkarni <onkark441@gmail.com>
2024-07-27 14:38:38 +0530

    my first project push to git

---
+++ src/main/java/com/shiva/myapp/Dev.java
@@ -0,0 +1,13 @@
+package com.shiva.myapp;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Dev {
+
+    public void man(){
+
+        System.out.println("Onkar's Spring");
+    }
+    
+}
